22e6fe841878898dc65c410f1e60bc4e0b9f90b2
mohikhsan <mohammadikhsan07@gmail.com>
2017-04-04 13:39:02 +0800

    Added label initiation to load_img_sequence

--- mainwindow.py
+++ mainwindow.py
@@ -2,7 +2,8 @@ from PyQt5.QtWidgets import QWidget, QApplication, QFileDialog, QTreeWidgetItem
 from ui_mainwindow import Ui_MainWindow
 
 from os import listdir, makedirs
-from os.path import isfile, join, basename
+from os.path import isfile, join, basename, exists
+from glob import glob
 
 class MainWindow(QWidget):
     def __init__(self, parent=None):
@@ -13,7 +14,9 @@ class MainWindow(QWidget):
         self.ui.setupUi(self)
 
         # Class variables
+        self.file_formats = ('.jpg','.png','.bmp','.gif')
         self.img_directory = None
+        self.table_db = None
 
         # Signals & Slots
         self.ui.btn_load_sequence.clicked.connect(self.load_img_dir)
@@ -22,25 +25,28 @@ class MainWindow(QWidget):
         """Callback for load directory button
 
         """
-        self.img_directory = QFileDialog.getExistingDirectory(self, "Select Image Directory", options = QFileDialog.ShowDirsOnly)
+        self.img_directory = QFileDialog.getExistingDirectory(self, 'Select Image Directory', options = QFileDialog.ShowDirsOnly)
 
-        col_headers = ['Filename', 'Labeled']
-        img_files = [f for f in listdir(self.img_directory) if isfile(join(self.img_directory,f))]
-        labels = []
+        if not self.img_directory == '':
+            col_headers = ['Filename', 'Labeled']
+            img_files = [f for f in listdir(self.img_directory) if f.endswith(self.file_formats)]
 
-        
+            if not exists(self.img_directory + '/labels'):
+                makedirs(self.img_directory + '/labels')
 
-        label_files = basename([f for f in listdir(self.img_directory + '/labels') if isfile(join(self.img_directory,f))])
-        for fname in img_files:
-            if basename(fname) in label_files:
-                labels.append(True)
-            else:
-                labels.append(False)
-        print(img_files)
-        print(label_files)
-        print(labels)
+            labels = []
+            label_files = [f for f in listdir(self.img_directory + '/labels') if isfile(join(self.img_directory,f))]
 
+            for i, mfile in enumerate(label_files):
+                label_files[i] = basename(mfile)
 
+            for fname in img_files:
+                if basename(fname) in label_files:
+                    labels.append(True)
+                else:
+                    labels.append(False)
+
+            self.table_db = [list(tup) for tup in list(zip(img_files,labels))]
 
 
 if __name__ == "__main__":
