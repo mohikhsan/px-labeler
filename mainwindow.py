b12b66c15159e684ff93fa383f6e149850465df3
mohikhsan <mohammadikhsan07@gmail.com>
2017-04-09 16:45:24 +0800

    Docs and clean up

--- mainwindow.py
+++ mainwindow.py
@@ -74,7 +74,14 @@ class MainWindow(QWidget):
         self.ui.main_display.setMouseTracking(True)
         self.ui.main_display.installEventFilter(self)
 
+    #########################
+    # Main Display functions
+    #########################
+
     def eventFilter(self, source, event):
+        """Callback to handle main display events
+
+        """
         if event.type() == QEvent.MouseMove and self.table_loaded:
             pos = event.pos()
             if event.buttons() == Qt.NoButton:
@@ -89,6 +96,55 @@ class MainWindow(QWidget):
 
         return QWidget.eventFilter(self, source, event)
 
+    def update_display(self, mouse_pos=None, cursor_flag=False):
+        """Updates main display when called
+
+        """
+        self.display_frame = self.img_frame.copy()
+        self.display_frame = cv2.addWeighted(self.display_frame, 1, self.pxlabel_frame, 0.5, 0)
+        if cursor_flag:
+            cv2.circle(self.display_frame, (mouse_pos.x(), mouse_pos.y()), self.cursor_size, self.cursor_color[::-1], 3)
+        self.ui.main_display.setPixmap(QPixmap.fromImage(self.cv2qimage(self.display_frame)))
+
+    def pxlabel2frame(self, img_size, pxlabel_mat, pxmarker_table):
+        """Creates pixel label display frame from pixel label matrix
+
+        """
+        pxlabel_frame_out = np.zeros(img_size, np.uint8)
+        for marker in pxmarker_table:
+            pxlabel_frame_out[pxlabel_mat == marker[0]] = marker[1]
+
+        return pxlabel_frame_out
+
+    def load_img_frame(self, img_filename):
+        """Load image frame from image file and set to main display pixmap
+
+        """
+        self.img_frame = cv2.imread(img_filename)
+        self.img_size = self.img_frame.shape
+        self.ui.main_display.setPixmap(QPixmap.fromImage(self.cv2qimage(self.img_frame)))
+
+    def cv2qimage(self,cv2_img):
+        """Transform cv2 np array image to QImage.
+
+        """
+        temp_img = cv2_img.copy()
+
+        height, width = temp_img.shape[:2]
+        byte_value = 3 * width
+
+        if len(temp_img.shape) == 3:
+            cv2.cvtColor(temp_img, cv2.COLOR_BGR2RGB, temp_img)
+            qImage_out = QImage(temp_img.data, width, height, byte_value, QImage.Format_RGB888)
+        else:
+            qImage_out = QImage(temp_img.repeat(3).tostring(), width, height, byte_value, QImage.Format_RGB888)
+
+        return qImage_out
+
+    #########################
+    # File table functions
+    #########################
+
     def on_load_img_dir(self):
         """Callback for load directory button
 
@@ -98,106 +154,6 @@ class MainWindow(QWidget):
         if not self.img_directory == '':
             self.table_loaded = self.load_img_table(self.img_directory)
             self.ui.table_filename.selectRow(0)
-
-    def on_table_selection_change(self):
-        """Callback for table selection change.
-
-        Updates image index, filename, frame, and frame label.
-
-        """
-        self.img_table_idx = self.ui.table_filename.currentRow()
-        self.img_filename = self.ui.table_filename.item(self.img_table_idx, 0).text()
-
-        self.load_img_frame(self.img_directory + '/' + self.img_filename)
-        self.ui.label_frame_num.setText(str(self.img_table_idx+1) + '/' +str(self.table_height))
-
-        self.pxlabel_filename = self.img_directory + '/labels/' + splitext(self.img_filename)[0] + '.pkl'
-        self.pxlabel_mat = self.load_pxlabel_mat(self.pxlabel_filename)
-        self.pxlabel_mat_ori = self.pxlabel_mat.copy()
-        self.pxlabel_frame = self.pxlabel2frame(self.img_size, self.pxlabel_mat, self.pxmarker_table)
-
-        self.update_display()
-
-    def on_next_frame_click(self):
-        if self.table_loaded:
-            if self.img_table_idx < self.table_height - 1:
-                current_row = self.img_table_idx + 1
-            else:
-                current_row = 0
-            self.ui.table_filename.selectRow(current_row)
-
-    def on_prev_frame_click(self):
-        if self.table_loaded:
-            if self.img_table_idx > 0:
-                current_row = self.img_table_idx - 1
-            else:
-                current_row = self.table_height - 1
-
-            self.ui.table_filename.selectRow(current_row)
-
-    def on_pxmarker_cbox_change(self, index):
-        self.pxmarker_current = self.pxmarker_table[index]
-        self.pxmarker_stylesheet = self.get_pxmarker_stylesheet(self.pxmarker_current[1])
-        self.ui.label_pxmarker_color.setStyleSheet(self.pxmarker_stylesheet)
-        self.cursor_color = self.pxmarker_current[1]
-
-    def on_pxmarker_edit_click(self):
-        pxmarker_dialog = PxMarkerDialog(self,self.pxmarker_table)
-
-        if pxmarker_dialog.exec():
-            print(self.pxmarker_table)
-            print(pxmarker_dialog.pxmarker_table_out)
-        else:
-            pass
-
-        pxmarker_dialog = None
-
-    def update_display(self, mouse_pos=None, cursor_flag=False):
-        self.display_frame = self.img_frame.copy()
-        self.display_frame = cv2.addWeighted(self.display_frame, 1, self.pxlabel_frame, 0.5, 0)
-        if cursor_flag:
-            cv2.circle(self.display_frame, (mouse_pos.x(), mouse_pos.y()), self.cursor_size, self.cursor_color[::-1], 3)
-        self.ui.main_display.setPixmap(QPixmap.fromImage(self.cv2qimage(self.display_frame)))
-
-    def load_pxmarker_table(self):
-        try:
-            with open('px_marker.pkl', 'rb') as f:
-                pxmarker_table = pickle.load(f)
-                return pxmarker_table
-        except EnvironmentError as e:
-            pxmarker_table = [
-                                [0, (0,0,0), 'Eraser'],
-                                [1, (255,0,0), 'Feature 1'],
-                                [2, (0,255,0), 'Feature 2'],
-                                [3, (0,0,255), 'Feature 3'],
-                                [4, (255,255,0), 'Feature 4'],
-                                [5, (0,255,255), 'Feature 5'],
-                                [6, (255,0,255), 'Feature 6'],
-                                [7, (128,128,0), 'Feature 7'],
-                                [8, (0,128,128), 'Feature 8'],
-                                [9, (128,0,128), 'Feature 9'],
-                                [10, (255,153,153), 'Feature 10']
-            ]
-
-            with open('px_marker.pkl', 'wb') as f:
-                pickle.dump(pxmarker_table, f)
-                return pxmarker_table
-
-    def save_pxmarker_table(self):
-        with open('px_marker.pkl', 'wb') as f:
-            pickle.dump(self.pxmarker_table, f)
-            print("Pxmarker pickled")
-
-    def get_pxmarker_stylesheet(self, color):
-        return "background-color: rgb" + str(color) + ";"
-
-    def update_pxmarker_cbox(self, pxmarker_table):
-        cbox_list = []
-        for item in pxmarker_table:
-            cbox_list.append(item[2])
-
-        self.ui.cbox_pxmarker_select.clear()
-        self.ui.cbox_pxmarker_select.addItems(cbox_list)
 
     def load_img_table(self, img_file_dir):
         """Create table of image files and pxlabel files
@@ -238,6 +194,133 @@ class MainWindow(QWidget):
 
         return True
 
+    def on_table_selection_change(self):
+        """Callback for table selection change.
+
+        Updates image index, filename, frame, and frame label.
+
+        """
+        self.img_table_idx = self.ui.table_filename.currentRow()
+        self.img_filename = self.ui.table_filename.item(self.img_table_idx, 0).text()
+
+        self.load_img_frame(self.img_directory + '/' + self.img_filename)
+        self.ui.label_frame_num.setText(str(self.img_table_idx+1) + '/' +str(self.table_height))
+
+        self.pxlabel_filename = self.img_directory + '/labels/' + splitext(self.img_filename)[0] + '.pkl'
+        self.pxlabel_mat = self.load_pxlabel_mat(self.pxlabel_filename)
+        self.pxlabel_mat_ori = self.pxlabel_mat.copy()
+        self.pxlabel_frame = self.pxlabel2frame(self.img_size, self.pxlabel_mat, self.pxmarker_table)
+
+        self.update_display()
+
+    def on_next_frame_click(self):
+        """Callback for next button click
+
+        """
+        if self.table_loaded:
+            if self.img_table_idx < self.table_height - 1:
+                current_row = self.img_table_idx + 1
+            else:
+                current_row = 0
+            self.ui.table_filename.selectRow(current_row)
+
+    def on_prev_frame_click(self):
+        """Callback for prev button click
+
+        """
+
+        if self.table_loaded:
+            if self.img_table_idx > 0:
+                current_row = self.img_table_idx - 1
+            else:
+                current_row = self.table_height - 1
+
+            self.ui.table_filename.selectRow(current_row)
+
+    #########################
+    # Drawing marker functions
+    #########################
+
+    def on_pxmarker_cbox_change(self, index):
+        """Callback for marker combobox change
+
+        """
+
+        self.pxmarker_current = self.pxmarker_table[index]
+        self.pxmarker_stylesheet = self.get_pxmarker_stylesheet(self.pxmarker_current[1])
+        self.ui.label_pxmarker_color.setStyleSheet(self.pxmarker_stylesheet)
+        self.cursor_color = self.pxmarker_current[1]
+
+    def on_pxmarker_edit_click(self):
+        """Callback for marker edit button click. Launches edit marker dialog.
+
+        """
+        pxmarker_dialog = PxMarkerDialog(self,self.pxmarker_table)
+
+        if pxmarker_dialog.exec():
+            print(self.pxmarker_table)
+            print(pxmarker_dialog.pxmarker_table_out)
+        else:
+            pass
+
+        pxmarker_dialog = None
+
+    def load_pxmarker_table(self):
+        """Initiates pixel marker table (from .pkl file if availble)
+
+        """
+        try:
+            with open('px_marker.pkl', 'rb') as f:
+                pxmarker_table = pickle.load(f)
+                return pxmarker_table
+        except EnvironmentError as e:
+            pxmarker_table = [
+                                [0, (0,0,0), 'Eraser'],
+                                [1, (255,0,0), 'Feature 1'],
+                                [2, (0,255,0), 'Feature 2'],
+                                [3, (0,0,255), 'Feature 3'],
+                                [4, (255,255,0), 'Feature 4'],
+                                [5, (0,255,255), 'Feature 5'],
+                                [6, (255,0,255), 'Feature 6'],
+                                [7, (128,128,0), 'Feature 7'],
+                                [8, (0,128,128), 'Feature 8'],
+                                [9, (128,0,128), 'Feature 9'],
+                                [10, (255,153,153), 'Feature 10']
+            ]
+
+            with open('px_marker.pkl', 'wb') as f:
+                pickle.dump(pxmarker_table, f)
+                return pxmarker_table
+
+    def save_pxmarker_table(self):
+        """Save edited pixel marker to .pkl file
+
+        """
+        with open('px_marker.pkl', 'wb') as f:
+            pickle.dump(self.pxmarker_table, f)
+            print("Pxmarker pickled")
+
+    def get_pxmarker_stylesheet(self, color):
+        """Create stylesheet for pixel marker color indicator
+
+        """
+        return "background-color: rgb" + str(color) + ";"
+
+    def update_pxmarker_cbox(self, pxmarker_table):
+        """Updates pixel marker combobox with edited values
+
+        """
+        cbox_list = []
+        for item in pxmarker_table:
+            cbox_list.append(item[2])
+
+        self.ui.cbox_pxmarker_select.clear()
+        self.ui.cbox_pxmarker_select.addItems(cbox_list)
+
+    #########################
+    # Label matrix functions
+    #########################
+
     def load_pxlabel_mat(self, pxlabel_filename):
         """Load pxlabel matrix if available, create one if not
 
@@ -257,48 +340,22 @@ class MainWindow(QWidget):
         if self.pxlabel_filename:
             self.pxlabel_mat = self.update_pxlabel_mat(self.pxlabel_mat, self.pxlabel_frame, self.pxmarker_table)
 
+            item = QTableWidgetItem(str(True))
+            self.ui.table_filename.setItem(self.img_table_idx,1,item)
+
             with open(self.pxlabel_filename, 'wb') as f:
                 pickle.dump(self.pxlabel_mat, f)
                 print("Pxlabel pickled")
 
     def update_pxlabel_mat(self, pxlabel_mat, pxlabel_frame, pxmarker_table):
+        """Updates the pixel label matrix based on values in the pixel label frame
+
+        """
         pxlabel_mat_out = pxlabel_mat.copy()
         for marker in pxmarker_table:
             pxlabel_mat_out[np.where((pxlabel_frame == marker[1]).all(axis=2))[:2]] = marker[0]
         return pxlabel_mat_out
 
-    def pxlabel2frame(self, img_size, pxlabel_mat, pxmarker_table):
-        pxlabel_frame_out = np.zeros(img_size, np.uint8)
-        for marker in pxmarker_table:
-            pxlabel_frame_out[pxlabel_mat == marker[0]] = marker[1]
-
-        return pxlabel_frame_out
-
-
-    def load_img_frame(self, img_filename):
-        """Load image frame from image file
-
-        """
-        self.img_frame = cv2.imread(img_filename)
-        self.img_size = self.img_frame.shape
-        self.ui.main_display.setPixmap(QPixmap.fromImage(self.cv2qimage(self.img_frame)))
-
-    def cv2qimage(self,cv2_img):
-        """Transform cv2 np array image to QImage.
-
-        """
-        temp_img = cv2_img.copy()
-
-        height, width = temp_img.shape[:2]
-        byte_value = 3 * width
-
-        if len(temp_img.shape) == 3:
-            cv2.cvtColor(temp_img, cv2.COLOR_BGR2RGB, temp_img)
-            qImage_out = QImage(temp_img.data, width, height, byte_value, QImage.Format_RGB888)
-        else:
-            qImage_out = QImage(temp_img.repeat(3).tostring(), width, height, byte_value, QImage.Format_RGB888)
-
-        return qImage_out
 
 if __name__ == "__main__":
     import sys
